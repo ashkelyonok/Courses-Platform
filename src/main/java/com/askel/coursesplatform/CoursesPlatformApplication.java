871a8fd926b04bd56e5e9d030e0a484bda58ffc3
ashkelyonok <anastasia.shkelyonok@gmail.com>
2025-02-17 23:31:50 +0300

    Initial Commit

---
+++ src/main/java/com/askel/coursesplatform/CoursesPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.askel.coursesplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesPlatformApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/askel/coursesplatform/model/Course.java
@@ -0,0 +1,9 @@
+package com.askel.coursesplatform.model;
+
+public class Course {
+    private String name;
+    private String description;
+    private String author;
+
+
+}
