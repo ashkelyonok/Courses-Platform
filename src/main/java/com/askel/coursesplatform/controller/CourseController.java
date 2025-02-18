7c0eef04f226f2d1443942eccecf4ef2b389a061
ashkelyonok <anastasia.shkelyonok@gmail.com>
2025-02-18 13:50:27 +0300

    Update CourseController.java
    changed "import org.springframework.web.bind.annotation.*;" to full description

--- src/main/java/com/askel/coursesplatform/controller/CourseController.java
+++ src/main/java/com/askel/coursesplatform/controller/CourseController.java
@@ -5,7 +5,11 @@ import com.askel.coursesplatform.service.CourseService;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 
