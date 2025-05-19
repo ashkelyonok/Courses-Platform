efa776f05b654263a1a02226d7b4f8795cd9a50d
ashkelyonok <anastasia.shkelyonok@gmail.com>
2025-05-19 22:52:34 +0300

    cors in config and yaml

--- backend/src/main/java/com/askel/coursesplatform/config/CorsConfig.java
+++ backend/src/main/java/com/askel/coursesplatform/config/CorsConfig.java
@@ -10,7 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                //.allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://courses-platform-front.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
